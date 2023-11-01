d4cfa9a4b371496110f6e0551adc8042718c04a2
princehabeeb <habiy3400@gmail.com>
2023-11-01 23:46:43 +0000

    new

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,22 +1,79 @@
 'use client'
-import { usePathname } from 'next/navigation'
-import Link from 'next/link'
+
+import { usePathname } from 'next/navigation';
+import Link from 'next/link';
+import Image from 'next/image';
+import Logo from '../images/Logo.png';
 
 const Nav = () => {
+    const pathname = usePathname();
   return (
-    <nav>
+    <nav className='h-[92px] flex items-center justify-between'>
+    <Image
+      className='logo_img'
+      src={Logo}
+      alt="Picture of the author"
+    />
       <ul>
         <li>
-          <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">
-            Home
+          <Link className={`link ${pathname === '/laptops' ? 'active' : ''}`} href="/laptops">
+          Laptops
           </Link>
         </li>
         <li>
           <Link
-            className={`link ${pathname === '/about' ? 'active' : ''}`}
-            href="/about"
+            className={`link ${pathname === '/desktops' ? 'active' : ''}`}
+            href="/desktops"
           >
-            About
+            Desktop PCs
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`link ${pathname === '/networking_devices' ? 'active' : ''}`}
+            href="/networking_devices"
+          >
+            Networking Devices
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`link ${pathname === '/printers_&_scanners' ? 'active' : ''}`}
+            href="/printers_&_scanners"
+          >
+            Printers & Scanners
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`link ${pathname === '/pc_parts' ? 'active' : ''}`}
+            href="/pc_parts"
+          >
+           PC Parts
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`link ${pathname === '/other' ? 'active' : ''}`}
+            href="/other"
+          >
+            All Other Products
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`link ${pathname === '/repairs' ? 'active' : ''}`}
+            href="/repairs"
+          >
+            Repairs
+          </Link>
+        </li>
+        <li>
+          <Link
+            className={`link ${pathname === '/our_deals' ? 'active' : ''}`}
+            href="/our_deals"
+          >
+            Our Deals
           </Link>
         </li>
       </ul>
